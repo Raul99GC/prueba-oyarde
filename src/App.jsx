083dfa912af11ce92305866e4174bcce2307ce86
Raul99GC <raulc99g@gmail.com>
2023-03-14 10:39:39 -0600

    adding DisruptSection component in the main

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-
+import React from 'react'
 import { Header } from "./components/Header/Header"
 import { Home } from "./components/Home/Home"
+import styles from './app.module.css'
+import { DisruptSection } from './components/DisruptSection/DisruptSection'
 
 function App() {
-
   return (
-    <>
+    <div className={styles.app}>
       <Header />
-      <main>
+      <main className={styles.main}>
         <Home />
+        <DisruptSection />
       </main>
 
-    </>
+    </div>
   )
 }
 
