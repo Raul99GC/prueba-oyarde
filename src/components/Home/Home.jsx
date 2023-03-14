2a624fec7d57dca99ee22d0998b2a7cced95c72a
Raul99GC <raulc99g@gmail.com>
2023-03-14 08:08:43 -0600

    finishing the home component but unly for mobile

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import styles from './home.module.css'
+import video from './assets/heroe.mp4'
 
 export const Home = () => {
   return (
     <section className={styles.home}>
       <div className={styles.infContainer}>
         <h1 className={styles.title}>Manufacturing digital solutions.</h1>
+        <p className={styles.infParagraph}>Custom digital products that will forever change the way you do business.</p>
+      </div>
+      <div className={styles.videoCont}>
+        <video autoPlay loop muted src={video} className={styles.video}></video>
       </div>
     </section>
   )
