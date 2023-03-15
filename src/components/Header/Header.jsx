4d1ed0eaa0b654a491044ea81b7f93cdda9a8ac3
Raul99GC <raulc99g@gmail.com>
2023-03-14 20:51:00 -0600

    adding a state for the button and adding the mobile menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './header.module.css'
 import logo from '../../assets/logo.png'
 import { FiMenu } from 'react-icons/fi'
@@ -6,14 +6,30 @@ import { FiMenu } from 'react-icons/fi'
 
 export const Header = () => {
 
+  const [buttonStatus, setButtonStatus] = useState(false)
+
+  const handleClick = () => {
+    setButtonStatus(!buttonStatus)
+  }
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <div className={styles.imgContainer}>
-          <img src={logo} alt="Factory Logo" />
+          <img className={styles.img} src={logo} alt="Factory Logo" />
         </div>
 
-        <button>
+        <nav className={`${styles.navMenu} ${buttonStatus&& styles.active}`}>
+          <ul className={styles.navContainer}>
+            <li className={styles.navItem}>ABOUT</li>
+            <li className={styles.navItem}>WORK</li>
+            <li className={styles.navItem}>CLIENTS</li>
+            <li className={styles.navItem}>BLOG</li>
+            <li className={styles.navItem}>CONTACT</li>
+          </ul>
+        </nav>
+
+        <button onClick={handleClick}>
           <FiMenu
             size={28}
             className={styles.mebuButton}
