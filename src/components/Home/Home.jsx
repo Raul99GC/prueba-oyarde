3e562c7a4388fa59ee28c7ada0cec46132405c9b
Raul99GC <raulc99g@gmail.com>
2023-03-15 10:29:31 -0600

    adding a container

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,12 +5,14 @@ import video from './assets/heroe.mp4'
 export const Home = () => {
   return (
     <section className={styles.home}>
-      <div className={styles.infContainer}>
-        <h1 className={styles.title}>Manufacturing digital solutions.</h1>
-        <p className={styles.infParagraph}>Custom digital products that will forever change the way you do business.</p>
-      </div>
-      <div className={styles.videoCont}>
-        <video autoPlay loop muted src={video} className={styles.video}></video>
+      <div className={styles.container}>
+        <div className={styles.infContainer}>
+          <h1 className={styles.title}>Manufacturing digital solutions.</h1>
+          <p className={styles.infParagraph}>Custom digital products that will forever change the way you do business.</p>
+        </div>
+        <div className={styles.videoCont}>
+          <video autoPlay loop muted src={video} className={styles.video}></video>
+        </div>
       </div>
     </section>
   )
